ac0dfd4f31d0c2aeda6f9334a6775cf54bdbf20e
Paweł Zuzelski <pawelz@pld-linux.org>
2009-09-03 15:54:32 +0200

    Copyright and license info

--- eviloverlord.sh
+++ eviloverlord.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -e
 
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+
+# Copyright (c) by Paweł Zuzelski <pawelz@pld-linux.org>
+
+# eviloverlord fortunes are distributable. See COPYING for details.
+
 BASEURL="http://www.eviloverlord.com/lists/"
 LISTS="{overlord.html,dungeon_a.html,dungeon_b.html}"
 
